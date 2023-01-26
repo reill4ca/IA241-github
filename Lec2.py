93e386d8eee3601ebb43d77ea45306527e3ae3e4
EC2 Default User <ec2-user@ip-172-31-10-243.ec2.internal>
2023-01-26 21:08:47 +0000

    lecture 2

---
+++ Lec2.py
@@ -0,0 +1,32 @@
+'''
+lec 2
+numbers, strings, and variables
+'''
+
+#print(  type('hello')  ) # single line comment
+
+#print(  type('123.')   )
+
+#print(  type(123.123)  )
+
+#print(   'hello ' )
+#print(   "hello "  )
+#print(  " it's 2nd python class  ")
+
+#print('Hello World')
+
+#print('Hello World'.lower())
+
+# print('   hello world  '.strip().lower())
+#print('hello world')
+#print('hello world'.split())  #default, split by spaces
+#print('hello world'.split('o')) # split by "o"
+
+#print('hello ' + 'world')
+#print(1/2)
+
+my_num = 123
+print(my_num)
+
+my_num = 234
+print(my_num)
