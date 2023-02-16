f3b5573f1de8994243e90a59628cd5e9e5e6eea3
EC2 Default User <ec2-user@ip-172-31-10-243.ec2.internal>
2023-02-16 21:52:02 +0000

    lec5 & lab5

---
+++ lab5.py
@@ -0,0 +1,39 @@
+'''
+lab5
+if statement
+'''
+
+#3.1
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('you get 5 points')
+    
+#3.2
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('you get 5 points')
+else:
+    print('you get 10 points')
+
+#3.3
+favorite_fruits = ['apple', 'banana', 'peach']
+if 'apple' in favorite_fruits:
+    print('you really like apples')
+if 'banana' in favorite_fruits:
+    print('you really like bananas')
+if 'peach' in favorite_fruits:
+    print('you really like peaches')
+    
+#3.4
+age = 45
+
+if age<10:
+    print('person is a kid')
+if 10<age<20:
+    print('person is a teenager')
+if age>20:
+    print('person is an adult')
+if 65<age:
+    print('person is an elder')
---
+++ lec5.py
@@ -0,0 +1,51 @@
+#import this
+
+#print(   1+15+
+#             4564)
+             
+#1  +  2 + \
+#12+456
+#a = 1
+#b = 1
+
+#print(id(a))
+#print(id(b))
+#print(id(1))
+
+#print( a == b)
+
+#print(a is b )
+
+#a = None
+
+#print(id(a))
+#print(id(None))
+
+#print(a is None)
+#print(a == None)
+
+#x = None
+#print(x is None)
+
+#print( not True )
+
+#print(1==2)
+
+#if 2>1 :
+ #   print('2>1')
+  #  print('another2>1')
+   # if 3>1:
+    #    print('3>1')
+#else:
+ #    print('else statement')
+    
+#print('out of if block')
+
+if 2<1:
+    print('2>1')
+    
+elif 3<1:
+    print('3>1')
+    
+else:
+    print('else')
