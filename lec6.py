72d0ed9dc72d2094a0eb467d37a81274d90db34a
EC2 Default User <ec2-user@ip-172-31-10-243.ec2.internal>
2023-02-23 21:55:39 +0000

    lab6 & lec6

---
+++ lec6.py
@@ -0,0 +1,27 @@
+'''
+lec 6
+for loop 
+range function
+'''
+#for num in [1,2,3]:
+    #if num >1:
+        #print(num)
+
+#for word in 'this is lec6'.split( ):
+    #print(word)
+    #for c in word:
+        #print(c)
+        
+#for i in range(1,5):
+ #   print(i)
+ 
+num_list = [123,434,43243,23432,4324]  
+
+possible_max_num = num_list[0]
+
+for num in num_list:
+    if num > possible_max_num:
+        possible_max_num = num 
+        
+print(possible_max_num)
+
